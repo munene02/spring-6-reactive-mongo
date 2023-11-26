a83df8cec86e3f1984fc4952ec460e7e3ce4b947
Martin Munene <munene02@gmail.com>
2023-11-26 14:41:36 +0300

    added domain, mapper, model for beer and customer

---
+++ src/main/java/com/martinmunene/reactivemongo/domain/Beer.java
@@ -0,0 +1,29 @@
+package com.martinmunene.reactivemongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author Martin Munene
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+
+    private Integer id;
+    private String beerName;
+    private String beerStyle;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+
+}
---
+++ src/main/java/com/martinmunene/reactivemongo/domain/Customer.java
@@ -0,0 +1,33 @@
+package com.martinmunene.reactivemongo.domain;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Martin Munene
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+    @Id
+    private Integer id;
+
+    @Size(max = 255)
+    private String customerName;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
