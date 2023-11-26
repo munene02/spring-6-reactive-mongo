e7edf0498e81c04fb15ad968be3a63ea78a74d08
Martin Munene <munene02@gmail.com>
2023-11-26 17:45:04 +0300

    implemented more crud methods abt tests

--- src/main/java/com/martinmunene/reactivemongo/repositories/BeerRepository.java
+++ src/main/java/com/martinmunene/reactivemongo/repositories/BeerRepository.java
@@ -2,9 +2,11 @@ package com.martinmunene.reactivemongo.repositories;
 
 import com.martinmunene.reactivemongo.domain.Beer;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 /**
  * @author Martin Munene
  */
 public interface BeerRepository extends ReactiveMongoRepository<Beer, String> {
+    Mono<Beer> findFirstByBeerName(String beerName);
 }
